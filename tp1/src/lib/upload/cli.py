f9051d59cf39b6bf0cc879bf5403924adf3e8211
kameha1011 <omarcitofc2001@gmail.com>
2026-04-13 13:33:39 -0300

    feat: upload cli

---
+++ tp1/src/lib/upload/cli.py
@@ -0,0 +1,14 @@
+import argparse
+from ..common.cli import set_connnection_args, set_logger_args, set_protocol, set_file_name
+
+def cli():
+    parser = argparse.ArgumentParser(
+        prog="download", 
+        description="A client to upload to a server.",
+        usage='%(prog)s [ - h ] [ - v | -q ] [ - H ADDR ] [ - p PORT ] [ - d FILEPATH ] [ - n FILENAME ] [ - r protocol ]')
+    set_connnection_args(parser)
+    set_logger_args(parser)
+    set_protocol(parser)
+    set_file_name(parser)
+    parser.add_argument("-s", "--src", help="source file path", required=True)
+    return parser.parse_args()
---
+++ tp1/src/upload.py
@@ -0,0 +1,7 @@
+from lib.upload.cli import cli
+
+def main():
+    args = cli()
+
+if __name__ == "__main__":
+    main()
