fbf155c9a557f53c6b7bd4dd0b38113dfcb23854
kameha1011 <omarcitofc2001@gmail.com>
2026-04-13 13:34:02 -0300

    feat: download cli

---
+++ tp1/src/download.py
@@ -0,0 +1,7 @@
+from lib.download.cli import cli
+
+def main():
+    args = cli()
+
+if __name__ == "__main__":
+    main()
---
+++ tp1/src/lib/download/cli.py
@@ -0,0 +1,14 @@
+import argparse
+from ..common.cli import set_logger_args, set_connnection_args, set_protocol, set_file_name
+
+def cli():
+    parser = argparse.ArgumentParser(
+        prog="download", 
+        description="A client to download previously uploaded files from a server.",
+        usage='%(prog)s [ - h ] [ - v | -q ] [ - H ADDR ] [ - p PORT ] [ - d FILEPATH ] [ - n FILENAME ] [ - r protocol ]')
+    set_connnection_args(parser)
+    set_logger_args(parser)
+    set_protocol(parser)
+    set_file_name(parser)
+    parser.add_argument("-d", "--dst", help="dest file path", required=True)
+    return parser.parse_args()
